cde328e43bcf1d67ebe636c51437ba717621fd46
sarath19-2000 <99311128+sarath19-2000@users.noreply.github.com>
2026-04-02 12:39:09 +0200

    Add files via upload

---
+++ Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EventManagementSystem
+{
+    class Event
+    {
+        public int Id;
+        public string Name;
+        public string Description;
+        public DateTime Date;
+        public string Location;
+    }
+}
